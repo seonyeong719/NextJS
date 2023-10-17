211c71e1d04bdcc29c5a91f8df5f18e4939a3b1a
seonyeong719 <sy904280@gmail.com>
2023-10-17 23:13:53 +0900

    :sparkles: Feat: 동적 라우팅 해보기

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return <h1>main</h1>;
+  return (
+    <>
+      <h1>main</h1>
+      <ul>
+        <li>서울</li>
+        <li>뉴욕</li>
+        <li>런던</li>
+      </ul>
+    </>
+  );
 }
