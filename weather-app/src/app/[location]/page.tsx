0aaa0b045b8ba670716b152d8ccb1de31f611367
seonyeong719 <sy904280@gmail.com>
2023-10-18 19:03:35 +0900

    :sparkles: Feat: router 컴포넌트

--- weather-app/src/app/[location]/page.tsx
+++ weather-app/src/app/[location]/page.tsx
@@ -1,3 +1,5 @@
+import HomeButton from "../components/homeButton";
+
 type Props = {
   params: {
     location: string;
@@ -6,9 +8,11 @@ type Props = {
 
 export default function Detail({ params }: Props) {
   const name = params.location === "seoul" ? "서울" : "";
+
   return (
     <>
       <h1>{name}의 3일 예보</h1>
+      <HomeButton />
     </>
   );
 }
