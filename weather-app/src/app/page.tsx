0c917e38097eda28a185d84ed10ce94847a92bc8
seonyeong719 <sy904280@gmail.com>
2023-10-18 23:33:31 +0900

    :sparkles: Feat: api 추가

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,7 +1,22 @@
 import Link from "next/link";
 import style from "./style.module.css";
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const getCurrentWeather = async () => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Seoul&aqi=no`
+  );
+
+  if (!res.ok) {
+    if (!res.ok) throw new Error("날씨 정보를 가져올 수 없습니다.");
+  }
+
+  return res.json;
+};
+
+export default async function Home() {
+  const res = await getCurrentWeather();
+  console.log(res);
   return (
     <>
       <h1>날씨 앱</h1>
