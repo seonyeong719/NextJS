ec61e6a8bb62348822874e0ec84423de1b3e1560
seonyeong719 <sy904280@gmail.com>
2023-10-22 21:09:15 +0900

    :sparkles: Feat: 메타데이터

--- weather-app/src/app/[location]/page.tsx
+++ weather-app/src/app/[location]/page.tsx
@@ -1,15 +1,26 @@
 import { getForecast } from "@/utils/getForecast";
 import HomeButton from "../components/homeButton";
 
+// 동적 메타데이터를 활성화 하기 위해 params를 이용할 수 있고,searchParams도 이용할 수 있다.
+
 type Props = {
   params: {
     location: string;
   };
+  searchParams: {
+    name: string;
+  };
 };
 
-export default async function Detail({ params }: Props) {
-  const name = params.location === "seoul" ? "서울" : "";
+export function generateMetadata({ searchParams }: Props) {
+  return {
+    title: `날씨 앱 - ${searchParams.name}`,
+    description: `${searchParams.name} 날씨를 알려드립니다.`,
+  };
+}
 
+export default async function Detail({ params, searchParams }: Props) {
+  const name = searchParams.name;
   const res = await getForecast(params.location);
 
   return (
